dc8fac85185f999ba405137c34ff652f3691d826
BeeRuz68z <116024978+BeeRuz68z@users.noreply.github.com>
2024-05-22 15:34:23 +0530

    Update urls.py

--- LibraryAPI/books/urls.py
+++ LibraryAPI/books/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('allbooks/',booklistview.as_view(),name='bookdetailview'),
     # path('author/<str:name>',authorfilterview.as_view(),name='authorfilterview'),
-    path('idbook/<int:id>',bookdetailview.as_view(),name='booklistview'),
-    path('createbook',createbook.as_view(),name='createbook'),
+    path('idbook/<int:id>/',bookdetailview.as_view(),name='booklistview'),
+    path('createbook/',createbook.as_view(),name='createbook'),
     path('onebooks/<int:id>',bookoneview.as_view(),name='allbooklistview')
 ]
